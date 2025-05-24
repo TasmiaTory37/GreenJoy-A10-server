b42a54923be1ac7618584e76cc1baef006c7121a
TasmiaTory37 <torytasmmiaakter@gmail.com>
2025-05-24 15:41:24 +0600

    some modification in index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run() {
 
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const gardenCollection = client.db('gardenDB').collection('ShareTips');
 
@@ -183,7 +183,7 @@ app.patch('/tip/like/:id', async (req, res) => {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
